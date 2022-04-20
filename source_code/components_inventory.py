1b2ba39b85fe714745d14f6086ce8f49610d3566
DanielePlacido <daniele.placido@polito.it>
2022-04-20 13:07:15 +0200

    chore: rename class.
    Class ComponentsInventory renamed to ComponentsCollection.
    Provided more descriptive docstring.

--- source_code/components_inventory.py
+++ source_code/components_inventory.py
@@ -1,8 +1,13 @@
-class ComponentsInventory:
-    """Class that allows to organize the components that build the conductor """
+class ComponentsCollection:
+    """Class that allows to organize the components that build the conductor in collection(s).
+    A collection is characterized by:
+        1) a list of objects (not necessary of the same kind);\n
+        2) the number of objects that make up the collection;\n
+        3) the collection name (optional parameter).
+    """
 
     def __init__(self, name=None):
-        """Make an istance of class ComponentsInventory.
+        """Make an istance of class ComponentsCollection.
 
         Args:
             name (str, optional): name of the collection. Defaults to None.
