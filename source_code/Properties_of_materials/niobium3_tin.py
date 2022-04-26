57f1e31c66283f31ce54498276d2e6e293ded451
DanielePlacido <daniele.placido@polito.it>
2022-04-26 09:11:05 +0200

    feat: evaluate tcs with scipy.optimize.bisect.
    Exploit the bisection method from scipy.optimize library to evaluate the current sharing temperature.
    
    Small refactor: access directily the numpy array of the first element of the tuple returned by np.nonzero().

--- source_code/Properties_of_materials/niobium3_tin.py
+++ source_code/Properties_of_materials/niobium3_tin.py
@@ -1,5 +1,6 @@
 # Importing python libraries and other funcions
 import numpy as np
+from scipy import optimize
 import warnings
 
 # Function BCNBSN starts here
@@ -38,22 +39,22 @@ def BCNBSN(T, EPSLON, TC0M, BC20M):
 
     T = np.array(T)
     EPSLON = np.array(EPSLON)
-    if len(T) == 1:
+    if T.size == 1:
         T = T * np.ones(EPSLON.shape)
-    if len(EPSLON) == 1:
+    if EPSLON.size == 1:
         EPSLON = EPSLON * np.ones(T.shape)
 
     BC = np.zeros(T.shape)  # variable initialization
 
     # %*NORMALISED TEMPERATURE T/TC0
     TLCASE = T / (TC0M * (SNBSN(EPSLON)) ** (1.0e0 / 3.0e0))
-    index = np.nonzero(TLCASE < 1.0)  # this is a tuple (cdp, 06/2020)
-    if index[0].size == 0:  # empty index array (cdp, 06/2020)
+    index = np.nonzero(TLCASE < 1.0)[0]  # this is a numpy array (cdp, 06/2020)
+    if index.size == 0:  # empty index array (cdp, 06/2020)
         return BC
 
     # CRITICAL FIELD BC2(T,EPSLON)
-    BC[index[0]] = (
-        BC20M * SNBSN(EPSLON[index[0]]) * (1.0e0 - TLCASE[index[0]] ** 1.52e0)
+    BC[index] = (
+        BC20M * SNBSN(EPSLON[index]) * (1.0e0 - TLCASE[index] ** 1.52e0)
     )
 
     return BC
@@ -168,7 +169,7 @@ def isobaric_specific_heat_nb3sn(T, TCS, TC, TC0):
     TCS = np.array(TCS)
     TC = np.array(TC)
 
-    if len(T) == 1:
+    if T.size == 1:
         T = T * np.ones(TC.shape)
 
     CPN = np.zeros(T.shape)
@@ -221,46 +222,44 @@ def isobaric_specific_heat_nb3sn(T, TCS, TC, TC0):
 
     # SUPERCONDUCTING COMPONENT OF CP
     # Find index in T such that T <= TC
-    ind_TCa = np.nonzero(T <= TC)
+    ind_TCa = np.nonzero(T <= TC)[0]
     # For those index evaluate TC/TC0 ratio, elsewere it is = 0 by initialization
-    TC_norm[ind_TCa[0]] = TC[ind_TCa[0]] / TC0
+    TC_norm[ind_TCa] = TC[ind_TCa] / TC0
     # For those index evaluate DBC2DT and DELCP, elsewere they are = 0 by initialization
 
-    DBC2DT[ind_TCa[0]] = -0.46306 - 0.067830 * TC[ind_TCa[0]]
-    DELCP[ind_TCa[0]] = (
+    DBC2DT[ind_TCa] = -0.46306 - 0.067830 * TC[ind_TCa]
+    DELCP[ind_TCa] = (
         1500.0
-        * (DBC2DT[ind_TCa[0]] ** 2)
-        / (2.0 * (27.2 / (1.0 + (0.34 * TC_norm[ind_TCa[0]]))) ** 2 - 1.0)
+        * (DBC2DT[ind_TCa] ** 2)
+        / (2.0 * (27.2 / (1.0 + (0.34 * TC_norm[ind_TCa]))) ** 2 - 1.0)
     )
     # For those index evaluate piecewise fuction CPNTC, elsewere it is = 0 by initialization
-    # CPNTC[ind_TCa[0]] = np.piecewise(TC[ind_TCa[0]], intervals_CPNTC, behavior_CPNTC)
+    # CPNTC[ind_TCa] = np.piecewise(TC[ind_TCa], intervals_CPNTC, behavior_CPNTC)
     # For those index evaluate CPS, elsewere it is = 0 by initialization
-    CPS[ind_TCa[0]] = (CPNTC[ind_TCa[0]] + DELCP[ind_TCa[0]]) * (
-        T[ind_TCa[0]] / TC[ind_TCa[0]]
+    CPS[ind_TCa] = (CPNTC[ind_TCa] + DELCP[ind_TCa]) * (
+        T[ind_TCa] / TC[ind_TCa]
     ) ** 3
 
     # COMPOSE THE COMPONENTS
     # Find index in T such that T <= TCS
-    ind_TCSa = np.nonzero(T <= TCS)  # this is a tuple (cdp, 06/2020)
+    ind_TCSa = np.nonzero(T <= TCS)[0]  # this is a numpy array (cdp, 06/2020)
     # For those index evaluate CPNBSN
-    CPNBSN[ind_TCSa[0]] = CPS[ind_TCSa[0]]
+    CPNBSN[ind_TCSa] = CPS[ind_TCSa]
     # Find index in T such that T > TCS and t <= TC
-    ind_inter = np.nonzero((T > TCS) & (T <= TC))
+    ind_inter = np.nonzero((T > TCS) & (T <= TC))[0]
     # Find index in TCS such that TCS < TC
-    IND = np.nonzero(
-        TCS[ind_inter[0]] < TC[ind_inter[0]]
-    )  # this is a tuple (cdp, 06/2020)
-    ind = ind_inter[0][IND[0]]  # this is an array (cdp, 06/2020)
+    IND = np.nonzero(TCS[ind_inter] < TC[ind_inter])  # this is a numpy array (cdp, 06/2020)
+    ind = ind_inter[IND]  # this is an array (cdp, 06/2020)
     # For those index evaluate F, elsewere it is = 1 by initialization
     F[ind] = (T[ind] - TCS[ind]) / (TC[ind] - TCS[ind])
     # For index in ind_inter evaluate CPNBSN
-    CPNBSN[ind_inter[0]] = (
-        F[ind_inter[0]] * CPN[ind_inter[0]]
-        + (1.0 - F[ind_inter[0]]) * CPS[ind_inter[0]]
+    CPNBSN[ind_inter] = (
+        F[ind_inter] * CPN[ind_inter]
+        + (1.0 - F[ind_inter]) * CPS[ind_inter]
     )
     # Find index in T such that T > TC
-    ind_TCb = np.nonzero(T > TC)  # this is a tuple (cdp, 06/2020)
-    CPNBSN[ind_TCb[0]] = CPN[ind_TCb[0]]
+    ind_TCb = np.nonzero(T > TC)[0]  # this is a numpy array (cdp, 06/2020)
+    CPNBSN[ind_TCb] = CPN[ind_TCb]
 
     return CPNBSN  # end of the function
 
@@ -318,10 +317,11 @@ def critical_current_density_nb3sn(T, B, EPSLON, TC0M, BC20M, C0):
 
     T = np.array(T)
     B = np.array(B)
+    EPSLON = np.array(EPSLON)
 
-    if len(T) == 1:
+    if T.size == 1:
         T = T * np.ones(B.shape)
-    if len(B) == 1:
+    if B.size == 1:
         B = B * np.ones(T.shape)
 
     JC = np.zeros(T.shape)  # JC initialization
@@ -334,18 +334,18 @@ def critical_current_density_nb3sn(T, B, EPSLON, TC0M, BC20M, C0):
     TLCASE = T / (TC0M * (SNBSN(EPSLON)) ** (1.0e0 / 3.0e0))
 
     # Find element index in TLCASE such that TLCASE < 1.0 (cdp, 06/2020)
-    TLCASE_ind = np.nonzero(TLCASE < 1.0)  # this is a tuple (cdp, 06/2020)
-    if TLCASE_ind[0].size == 0:  # empty index array (cdp, 06/2020)
+    TLCASE_ind = np.nonzero(TLCASE < 1.0)[0]  # this is a numpy array (cdp, 06/2020)
+    if TLCASE_ind.size == 0:  # empty index array (cdp, 06/2020)
         return JC
 
     # NORMALISED FIELD B/BC0
     # Evaluate BLCASE only for those index, elsewere its value is 0.0 by initialization (cdp, 06/2020)
-    BLCASE[TLCASE_ind[0]] = BLIM[TLCASE_ind[0]] / (
-        BCNBSN(T[TLCASE_ind[0]], EPSLON[TLCASE_ind[0]], TC0M, BC20M)
+    BLCASE[TLCASE_ind] = BLIM[TLCASE_ind] / (
+        BCNBSN(T[TLCASE_ind], EPSLON[TLCASE_ind], TC0M, BC20M)
     )
     # Find element index such that BLCASE[TLCASE_ind[0]] < 1.0, only for those index JC will be evaluated, elsewere JC = 0.0 by initialization (cdp, 06/2020)
-    ind = np.nonzero(BLCASE[TLCASE_ind[0]] < 1.0)  # this is a tuple (cdp, 06/2020)
-    BLCASE_ind = TLCASE_ind[0][ind[0]]  # this is an array (cdp, 06/2020)
+    ind = np.nonzero(BLCASE[TLCASE_ind] < 1.0)[0]  # this is a numpy array (cdp, 06/2020)
+    BLCASE_ind = TLCASE_ind[ind]  # this is an array (cdp, 06/2020)
     if BLCASE_ind.size == 0:  # empty index array (cdp, 06/2020)
         return JC
 
@@ -361,8 +361,8 @@ def critical_current_density_nb3sn(T, B, EPSLON, TC0M, BC20M, C0):
         * (1 - BLCASE[BLCASE_ind]) ** qqq
     )
     # Find element index such that JC[BLCASE_ind] < 0, for these index JC = 0.0 (cdp, 2020)
-    ind = np.nonzero(JC[BLCASE_ind] < 0.0)  # this is a tuple (cdp, 06/2020)
-    JC_ind = BLCASE_ind[ind[0]]  # this is an array (cdp, 06/2020)
+    ind = np.nonzero(JC[BLCASE_ind] < 0.0)[0]  # this is a numpy array (cdp, 06/2020)
+    JC_ind = BLCASE_ind[ind]  # this is an array (cdp, 06/2020)
     JC[JC_ind] = 0.0
 
     return JC  # end of the function
@@ -478,13 +478,13 @@ def critical_temperature_nb3sn(B, EPSLON, TC0M, BC20M):
     # *NORMALISED FIELD
     BLCASE = BLIM / (BC20M * SNBSN(EPSLON))
 
-    index = np.nonzero(BLCASE < 1.0)  # this is a tuple (cdp, 06/2020)
-    if index[0].size == 0:  # empty index array (cdp, 06/2020)
+    index = np.nonzero(BLCASE < 1.0)[0]  # this is a numpy array (cdp, 06/2020)
+    if index.size == 0:  # empty index array (cdp, 06/2020)
         return TC
-    TC[index[0]] = (
+    TC[index] = (
         TC0M
-        * (SNBSN(EPSLON[index[0]])) ** (1.0e0 / 3.0e0)
-        * (1.0e0 - BLCASE[index[0]]) ** (1.0 / 1.52)
+        * (SNBSN(EPSLON[index])) ** (1.0e0 / 3.0e0)
+        * (1.0e0 - BLCASE[index]) ** (1.0 / 1.52)
     )
 
     return TC  # end of the function
@@ -532,9 +532,8 @@ def current_sharing_temperature_nb3sn(B, EPSLON, JOP, TC0M, BC20M, C):
         RS = C / BC20M * (1.0 - TT ** 2.0) * BB ** (ppp - 1.0) * (1.0 - BB) ** qqq - JJ
         return RS  # end of the function
 
-    def FF(TT, BB, EPSLON, JOP, TC0M, BC20M, C):
-        f0 = critical_current_density_nb3sn(TT, BB, EPSLON, TC0M, BC20M, C) - JOP
-        return f0
+    def critical_current_density_bisection_nb3sn(TT, BB, EPSLON, JOP, TC0M, BC20M, C):
+        return critical_current_density_nb3sn([TT], [BB], [EPSLON], TC0M, BC20M, C) - JOP
 
     # ppp = 0.56
     # qqq = 1.75
@@ -550,7 +549,7 @@ def current_sharing_temperature_nb3sn(B, EPSLON, JOP, TC0M, BC20M, C):
     BLOW = 0.01
 
     B = np.array(B)
-    if len(B) == 1:
+    if B.size == 1:
         B = B * np.ones(EPSLON.shape)
 
     # variable initialization
@@ -565,22 +564,22 @@ def current_sharing_temperature_nb3sn(B, EPSLON, JOP, TC0M, BC20M, C):
     Bstar = BLIM / BC20M / SNBSN(EPSLON)
     # * CHECK THAT THE FIELD IS BELOW THE UPPER CRITICAL VALUE
     # Find element index in such that Bstar < 1.0 (cdp, 06/2020)
-    Bstar_ind = np.nonzero(Bstar < 1.0)  # this is a tuple (cdp, 06/2020)
-    if Bstar_ind[0].size == 0:  # empty index array (cdp, 06/2020)
+    Bstar_ind = np.nonzero(Bstar < 1.0)[0]  # this is a numpy array (cdp, 06/2020)
+    if Bstar_ind.size == 0:  # empty index array (cdp, 06/2020)
         return TCS
     # Evaluate JC @ T = 0 only for elements corresponding to Bstar_ind, elsewere JC = 0.0 by initialization (cdp, 06/2020)
-    JC[Bstar_ind[0]] = critical_current_density_nb3sn(
-        np.zeros(B[Bstar_ind[0]].shape),
-        B[Bstar_ind[0]],
-        EPSLON[Bstar_ind[0]],
+    JC[Bstar_ind] = critical_current_density_nb3sn(
+        np.zeros(B[Bstar_ind].shape),
+        B[Bstar_ind],
+        EPSLON[Bstar_ind],
         TC0M,
         BC20M,
         C,
     )
     # * CHECK THAT JOP IS BELOW THE UPPER CRITICAL VALUE
-    # Find element index such that JC[Bstar_ind] < JOP (cdp, 06/2020)
-    ind = np.nonzero(JC[Bstar_ind[0]] > JOP)  # this is a tuple (cdp, 06/2020)
-    JC_ind = Bstar_ind[0][ind[0]]  # this is an array (cdp, 06/2020)
+    # Find element index such that 0 < JOP < JC[Bstar_ind] (cdp, 06/2020)
+    ind = np.nonzero((JOP[Bstar_ind] < JC[Bstar_ind]) & (JOP[Bstar_ind] > 0.0))[0]  # this is a numpy array (cdp, 06/2020)
+    JC_ind = Bstar_ind[ind]  # this is an array (cdp, 06/2020)
     if JC_ind.size == 0:
         return TCS
 
@@ -619,62 +618,80 @@ def current_sharing_temperature_nb3sn(B, EPSLON, JOP, TC0M, BC20M, C):
     # FIND CURRENT SHARING TEMPERATURE WITH BISECTION (cdp, 09/2020)
 
     T_upper = critical_temperature_nb3sn(B[JC_ind], EPSLON[JC_ind], TC0M, BC20M)
-    T_lower = np.array([0.0])
-    max_iter = 1000
-    tol = 9e-16
-    residual = np.zeros(len(JC_ind))
-    iteration = np.zeros(len(JC_ind), dtype=int)
-    inter = (T_upper - T_lower) / 2.0
 
-    F_upper_v = FF(T_upper, B[JC_ind], EPSLON[JC_ind], JOP, TC0M, BC20M, C)
-    F_lower = FF(T_lower, B[JC_ind], EPSLON[JC_ind], JOP, TC0M, BC20M, C)
-    F_prod = F_upper_v * F_lower
-    ind_F_prod = np.nonzero(F_prod > 0.0)[0]
-    if ind_F_prod.size > 0:
-        raise ValueError(
-            "ERROR! The sign of the function at the boundary of the \
-    interval [A,B] must be different!\n"
-        )
-    ind_F_upper_v = np.nonzero(F_upper_v == 0.0)[0]
-    if ind_F_upper_v.size > 0:
-        TCS[JC_ind[ind_F_upper_v]] = T_upper[ind_F_upper_v]
-    ind_F_lower = np.nonzero(F_lower == 0.0)[0]
-    if ind_F_lower.size > 0:
-        TCS[JC_ind[ind_F_lower]] = T_lower
+    for ii, vv in enumerate(ind):
 
-    ind_still = np.nonzero(TCS[JC_ind] == 0.0)[0]
-    for ii in range(len(ind_still)):
-        xx = np.array(
-            [float(T_lower), float((T_lower + T_upper[ii]) / 2.0), T_upper[ii]]
+        T_lower = np.array([3.5])
+
+        ex_args = (
+            B[vv], EPSLON[vv], JOP[vv], TC0M, BC20M, C
         )
-        BB = np.array([B[ind_still[ii]]])
-        EPS = np.array([EPSLON[ind_still[ii]]])
-        F_xx = FF(xx, BB, EPS, JOP, TC0M, BC20M, C)
-        while inter[ii] >= tol and iteration[ii] < max_iter:
-            iteration[ii] = iteration[ii] + 1
-            if F_xx[0] * F_xx[1] < 0:
-                xx[2] = xx[1]
-                xx[1] = (xx[0] + xx[2]) / 2.0
-                F_xx = FF(xx, BB, EPS, JOP, TC0M, BC20M, C)
-                inter[ii] = (xx[2] - xx[0]) / 2.0
-            elif F_xx[1] * F_xx[2] < 0:
-                xx[0] = xx[1]
-                xx[1] = (xx[0] + xx[2]) / 2.0
-                F_xx = FF(xx, BB, EPS, JOP, TC0M, BC20M, C)
-                inter[ii] = (xx[2] - xx[0]) / 2.0
-            else:
-                xx[1] = xx[np.nonzero(F_xx == 0.0)[0]]
-                inter[ii] = 0.0
-            # end if
-        # end while
-        if iteration[ii] == max_iter and inter[ii] > tol:
-            warnings.warn(
-                f"The bisection method stopped without satisfying the required tolerance {tol}, having reached the maximum number of iterations {max_iter}.\n"
-            )
-        TCS[ind_still[ii]] = xx[1]
-        xx = np.array([xx[1]])
-        residual[ii] = FF(xx, BB, EPS, JOP, TC0M, BC20M, C)
-    # end for
+        # Evaluate current sharing temperature with bisection method.
+        TCS[vv] = optimize.bisect(
+            critical_current_density_bisection_nb3sn,
+            T_lower,
+            T_upper[ii],
+            ex_args,
+            xtol=1e-5,
+        )
+    # End for ii.
+
+    # T_lower = np.array([3.5])
+    # max_iter = 1000
+    # tol = 9e-16
+    # residual = np.zeros(len(JC_ind))
+    # iteration = np.zeros(len(JC_ind), dtype=int)
+    # inter = (T_upper - T_lower) / 2.0
+
+    # F_upper_v = FF(T_upper, B[JC_ind], EPSLON[JC_ind], JOP, TC0M, BC20M, C)
+    # F_lower = FF(T_lower, B[JC_ind], EPSLON[JC_ind], JOP, TC0M, BC20M, C)
+    # F_prod = F_upper_v * F_lower
+    # ind_F_prod = np.nonzero(F_prod > 0.0)[0]
+    # if ind_F_prod.size > 0:
+    #     raise ValueError(
+    #         "ERROR! The sign of the function at the boundary of the \
+    # interval [A,B] must be different!\n"
+    #     )
+    # ind_F_upper_v = np.nonzero(F_upper_v == 0.0)[0]
+    # if ind_F_upper_v.size > 0:
+    #     TCS[JC_ind[ind_F_upper_v]] = T_upper[ind_F_upper_v]
+    # ind_F_lower = np.nonzero(F_lower == 0.0)[0]
+    # if ind_F_lower.size > 0:
+    #     TCS[JC_ind[ind_F_lower]] = T_lower
+
+    # ind_still = np.nonzero(TCS[JC_ind] == 0.0)[0]
+    # for ii in range(len(ind_still)):
+    #     xx = np.array(
+    #         [float(T_lower), float((T_lower + T_upper[ii]) / 2.0), T_upper[ii]]
+    #     )
+    #     BB = np.array([B[ind_still[ii]]])
+    #     EPS = np.array([EPSLON[ind_still[ii]]])
+    #     F_xx = FF(xx, BB, EPS, JOP, TC0M, BC20M, C)
+    #     while inter[ii] >= tol and iteration[ii] < max_iter:
+    #         iteration[ii] = iteration[ii] + 1
+    #         if F_xx[0] * F_xx[1] < 0:
+    #             xx[2] = xx[1]
+    #             xx[1] = (xx[0] + xx[2]) / 2.0
+    #             F_xx = FF(xx, BB, EPS, JOP, TC0M, BC20M, C)
+    #             inter[ii] = (xx[2] - xx[0]) / 2.0
+    #         elif F_xx[1] * F_xx[2] < 0:
+    #             xx[0] = xx[1]
+    #             xx[1] = (xx[0] + xx[2]) / 2.0
+    #             F_xx = FF(xx, BB, EPS, JOP, TC0M, BC20M, C)
+    #             inter[ii] = (xx[2] - xx[0]) / 2.0
+    #         else:
+    #             xx[1] = xx[np.nonzero(F_xx == 0.0)[0]]
+    #             inter[ii] = 0.0
+    #         # end if
+    #     # end while
+    #     if iteration[ii] == max_iter and inter[ii] > tol:
+    #         warnings.warn(
+    #             f"The bisection method stopped without satisfying the required tolerance {tol}, having reached the maximum number of iterations {max_iter}.\n"
+    #         )
+    #     TCS[ind_still[ii]] = xx[1]
+    #     xx = np.array([xx[1]])
+    #     residual[ii] = FF(xx, BB, EPS, JOP, TC0M, BC20M, C)
+    # # end for
     return TCS  # end of the function
 
 
