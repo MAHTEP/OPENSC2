02a397fa8c704e3922b84d481554d8cfdc80a5b2
DanielePlacido <daniele.placido@polito.it>
2022-05-11 14:34:28 +0200

    feat!: new methods in class CylindricalHelix.
    New private methods __eval_radius and __eval_alpha to evaluate the radius of the cylinder and the angle in the xy plane starting from the coordinates of the barycenter.
    New method helix_parametrization to evaluate x, y and z spatial coordinates of the helix given the angluar discretization (tau).
    
    Some updates in the docstring.

--- source_code/cylindrical_helix.py
+++ source_code/cylindrical_helix.py
@@ -1,25 +1,31 @@
 import numpy as np
 
+
 class CylindricalHelix:
     """Class with methods and attributes to evaluate some cylindrical helix geometrical information."""
 
-    def __init__(self, radius: float, heigth: float, costheta: float):
+    def __init__(self, x0: float, y0: float, heigth: float, costheta: float):
         """Method that make instances of class Helix. Allow to define an helix with some of the main geometrical information such as:
+        * radius;
+        * pitch angle;
         * reduced pitch;
         * number of windings
         * length of a single winding
         * total helix length
 
         Args:
-            radius (float): radius of the cylinder around which the helix wounds.
+            x0 (float): abscissa of the barycenter of the generic  StrandMixedComponent or StrandStabilizerComponent or StrandSuperconductorComponent object.
+            y0 (float): ordinate of the barycenter of the generic  StrandMixedComponent or StrandStabilizerComponent or StrandSuperconductorComponent object.
             heigth (float): heigth of the cylinder around which the helix wounds.
             costheta (float): cosine of the pitch angle of the helix.
         """
-        # Radius of the helix.
-        self.radius = radius
         # Height of the helix.
         self.height = heigth
-        # Evaluate theta.
+        # Evaluate cylinder radius
+        self.__eval_radius(x0, y0)
+        # Evaluate angle in the xy plane.
+        self.__eval_alpha(x0, y0)
+        # Evaluate pitch angle.
         self.__eval_theta(costheta)
         # Evaluate reduced_pitch.
         self.__eval_reduced_pitch()
@@ -30,9 +36,26 @@ class CylindricalHelix:
         # Evaluate the total lenght of the helix.
         self.__eval_helix_length()
 
+    def __eval_radius(self, x0: float, y0: float):
+        """Private method that evaluates the radius of the cylinder around which the helix wounds starting from x0 and y0 coordinates (the coorinates of the barycenter of the generic StrandMixedComponent or StrandStabilizerComponent or StrandSuperconductorComponent object). Value is assigned to attribute self.radius.
+
+        Args:
+            x0 (float): abscissa of the barycenter of the generic  StrandMixedComponent or StrandStabilizerComponent or StrandSuperconductorComponent object.
+            y0 (float): ordinate of the barycenter of the generic  StrandMixedComponent or StrandStabilizerComponent or StrandSuperconductorComponent object.
+        """
+        self.radius = np.sqrt(x0 ** 2 + y0 ** 2)
+
+    def __eval_alpha(self, x0: float, y0: float):
+        """Private method that evaluates the angle in the xy plane; used in helix parametrization. Value is assigned to attribute self.alpha.
+
+        Args:
+            x0 (float): abscissa of the barycenter of the generic  StrandMixedComponent or StrandStabilizerComponent or StrandSuperconductorComponent object.
+            y0 (float): ordinate of the barycenter of the generic  StrandMixedComponent or StrandStabilizerComponent or StrandSuperconductorComponent object.
+        """
+        self.alpha = np.arctan2(y0, x0)
 
     def __eval_theta(self, costheta: float):
-        """Private method that evaluate the pitch angle of the helix. Value is assigned to attribute self.theta.
+        """Private method that evaluates the pitch angle of the helix. Value is assigned to attribute self.theta.
 
         Args:
             costheta (float): cosin of the pitch angle.
@@ -49,11 +72,25 @@ class CylindricalHelix:
         self.winding_number = self.height / (2 * np.pi * self.reduced_pitch)
 
     def __eval_winding_length(self):
-        """Private method that evaluate the length of a single winding of the helix. Value is assigned to attribute self.winding_length."""
+        """Private method that evaluates the length of a single winding of the helix. Value is assigned to attribute self.winding_length."""
         self.winding_length = (
             2 * np.pi * np.sqrt(self.radius ** 2 + self.reduced_pitch ** 2)
         )
 
     def __eval_helix_length(self):
-        """Private method that evaluate the total length of the helix. Value is assigned to attribute self.length."""
+        """Private method that evaluates the total length of the helix. Value is assigned to attribute self.length."""
         self.length = self.windings_number * self.winding_length
+
+    def helix_parametrization(self, tau: np.ndarray) -> tuple:
+        """Method that evaluates cylindrical helix coordinates according to canonical parametrization.
+
+        Args:
+            tau (np.ndarray): angular discretization.
+
+        Returns:
+            tuple: x, y and z coordinate of the cylindrical helix.
+        """
+        xx = self.radius * np.cos(self.alpha + tau)
+        yy = self.radius * np.sin(self.alpha + tau)
+        zz = self.reduced_pitch * tau
+        return xx, yy, zz
