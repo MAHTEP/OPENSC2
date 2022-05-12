9e7eff528cbeae4c0d0ed86be8cac0a002035f32
DanielePlacido <daniele.placido@polito.it>
2022-05-12 13:28:06 +0200

    feat: define parent logger.

--- source_code/simulation_starter.py
+++ source_code/simulation_starter.py
@@ -1,6 +1,16 @@
+import logging
+import logging.config
+
 from opensc2_gui import OPENSC2_GUI
 
 # from opensc2_gui_simpl import OPENSC2_GUI
+
+logging.config.fileConfig(
+    fname="logging_opensc2.conf", disable_existing_loggers=True
+)
+
+# Get the logger specified in the file, this will be the parent logger.
+logger = logging.getLogger("opensc2Logger")
 
 # make an instance of class OPENSC2_GUI (cdp, 12/2020)
 gui = OPENSC2_GUI()
