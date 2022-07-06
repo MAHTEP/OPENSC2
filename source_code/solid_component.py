8aaede2920edc418f12127d7edbccd7e65c4af6b
DanielePlacido <daniele.placido@polito.it>
2022-07-06 15:39:18 +0200

    chore: comment property import in module solid_component.py.

--- source_code/solid_component.py
+++ source_code/solid_component.py
@@ -14,81 +14,81 @@ from utility_functions.auxiliary_functions import (
     do_interpolation,
 )
 
-# Cu properties
-from properties_of_materials.copper import (
-    thermal_conductivity_cu_nist,
-    isobaric_specific_heat_cu_nist,
-    density_cu,
-    electrical_resistivity_cu_nist,
-)
+# # Cu properties
+# from properties_of_materials.copper import (
+#     thermal_conductivity_cu_nist,
+#     isobaric_specific_heat_cu_nist,
+#     density_cu,
+#     electrical_resistivity_cu_nist,
+# )
 
-# NbTi properties
-from properties_of_materials.niobium_titanium import (
-    thermal_conductivity_nbti,
-    isobaric_specific_heat_nbti,
-    density_nbti,
-)
+# # NbTi properties
+# from properties_of_materials.niobium_titanium import (
+#     thermal_conductivity_nbti,
+#     isobaric_specific_heat_nbti,
+#     density_nbti,
+# )
 
-# Nb3Sn properties
-from properties_of_materials.niobium3_tin import (
-    thermal_conductivity_nb3sn,
-    isobaric_specific_heat_nb3sn,
-    density_nb3sn,
-)
+# # Nb3Sn properties
+# from properties_of_materials.niobium3_tin import (
+#     thermal_conductivity_nb3sn,
+#     isobaric_specific_heat_nb3sn,
+#     density_nb3sn,
+# )
 
-# RE123 properties
-from properties_of_materials.rare_earth_123 import (
-    thermal_conductivity_re123,
-    isobaric_specific_heat_re123,
-    density_re123,
-)
+# # RE123 properties
+# from properties_of_materials.rare_earth_123 import (
+#     thermal_conductivity_re123,
+#     isobaric_specific_heat_re123,
+#     density_re123,
+# )
 
-# Stainless steel properties
-from properties_of_materials.stainless_steel import (
-    thermal_conductivity_ss,
-    isobaric_specific_heat_ss,
-    density_ss,
-    electrical_resistivity_ss,
-)
+# # Stainless steel properties
+# from properties_of_materials.stainless_steel import (
+#     thermal_conductivity_ss,
+#     isobaric_specific_heat_ss,
+#     density_ss,
+#     electrical_resistivity_ss,
+# )
 
-# Glass-epoxy properties
-from properties_of_materials.glass_epoxy import (
-    thermal_conductivity_ge,
-    isobaric_specific_heat_ge,
-    density_ge,
-)
+# # Glass-epoxy properties
+# from properties_of_materials.glass_epoxy import (
+#     thermal_conductivity_ge,
+#     isobaric_specific_heat_ge,
+#     density_ge,
+# )
 
-# Silver properties
-from properties_of_materials.silver import (
-    thermal_conductivity_ag,
-    isobaric_specific_heat_ag,
-    density_ag,
-    electrical_resistivity_ag,
-)
+# # Silver properties
+# from properties_of_materials.silver import (
+#     thermal_conductivity_ag,
+#     isobaric_specific_heat_ag,
+#     density_ag,
+#     electrical_resistivity_ag,
+# )
 
-# HASTELLOY - C276 properties
-from properties_of_materials.hastelloy_c276 import (
-    thermal_conductivity_hc276,
-    isobaric_specific_heat_hc276,
-    density_hc276,
-    electrical_resistivity_hc276,
-)
+# # HASTELLOY - C276 properties
+# from properties_of_materials.hastelloy_c276 import (
+#     thermal_conductivity_hc276,
+#     isobaric_specific_heat_hc276,
+#     density_hc276,
+#     electrical_resistivity_hc276,
+# )
 
-# Solder Sn60Pb40 properties
-from properties_of_materials.solder_sn60_pb40 import (
-    thermal_conductivity_sn60pb40,
-    isobaric_specific_heat_sn60pb40,
-    density_sn60pb40,
-    electrical_resistivity_sn60pb40,
-)
+# # Solder Sn60Pb40 properties
+# from properties_of_materials.solder_sn60_pb40 import (
+#     thermal_conductivity_sn60pb40,
+#     isobaric_specific_heat_sn60pb40,
+#     density_sn60pb40,
+#     electrical_resistivity_sn60pb40,
+# )
 
-# Aluminium properties
-from properties_of_materials.aluminium import (
-    thermal_conductivity_al,
-    isobaric_specific_heat_al,
-    density_al,
-    electrical_resistivity_al,
-)
+# # Aluminium properties
+# from properties_of_materials.aluminium import (
+#     thermal_conductivity_al,
+#     isobaric_specific_heat_al,
+#     density_al,
+#     electrical_resistivity_al,
+# )
 
 
 class SolidComponent:
