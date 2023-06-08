df2b75b1fbfadc1a60336558e044503cb346a896
Daniele Placido <daniele.placido@polito.it>
2023-06-08 15:02:35 +0200

    feat: new methods for BC in class FluidComponent
    New methods that applyes boundary conditions for the thermal hydraulic problem in class FluidComponent.
    
    Method impose_pressure_drop
        Method that imposes a pressure drop as boundary conditions for the thermal hydraulic problem. Imposed quantities are:
            * inlet pressure
            * inlet temperature (or outlet temperature if back flow)
            * outlet pressure
        The method suitably accounts for forward (left to right)/backward (right to left) flow as well as for back flow (part of the fluid moves towards the inlet due to a pressure build up).
    
    Method impose_inl_p_out_v
        Method that imposes a inlet pressure and outlet velocity as boundary conditions for the thermal hydraulic problem. Imposed quantities are:
            * inlet pressure
            * inlet temperature (or outlet temperature if backflow)
            * outlet velocity
        The method suitably accounts for forward (left to right)/backward (right to left) flow as well as for back flow (part of the fluid moves towards the inlet due to a pressure build up).
    
    Method impose_inl_v_out_p
        Method that imposes a inlet velocity and outlet pressure as boundary conditions for the thermal hydraulic problem. Imposed quantities are:
            * inlet velocity
            * inlet temperature (or outlet temperature if backflow)
            * outlet pressure
        The method suitably accounts for forward (left to right)/backward (right to left) flow as well as for back flow (part of the fluid moves towards the inlet due to a pressure build up).
    
    modified:   fluid_component.py

--- source_code/fluid_component.py
+++ source_code/fluid_component.py
@@ -16,6 +16,7 @@ properties:
 import pandas as pd
 from collections import namedtuple
 from typing import Union, NamedTuple
+from utility_functions.auxiliary_functions import get_from_xlsx
 
 class FluidComponentInput:
     """Interface class used to get the input data for fluid components objects. Attributes inputs and operations are inherited from this class by Coolant and Channel class. Being an interface, this class has only the constructror (__init__) method."""
@@ -162,5 +163,345 @@ class FluidComponent:
                 backward=eq_idx.temperature, # first node
             ),
         )
+
+
+    def impose_pressure_drop(
+        self,
+        ndarrays:tuple,
+        conductor:object,
+        path:str,
+        )->tuple:
+        
+        """Method that imposes a pressure drop as boundary conditions for the thermal hydraulic problem. Imposed quantities are:
+            * inlet pressure
+            * inlet temperature (or outlet temperature if back flow)
+            * outlet pressure
+        The method suitably accounts for forward (left to right)/backward (right to left) flow as well as for back flow (part of the fluid moves towards the inlet due to a pressure build up).
+
+        Args:
+            ndarrays (tuple): collection of known term vector (Known) and system matrix (SYSMAT) np.ndarray on wich impose the boundary conditions.
+            conductor (Conductor): object with all the information of the conductor.
+            path (str): path of the auxiliary input file with the values for the boundary conditions (if INTIAL = -1).
+
+        Returns:
+            tuple: collection of known term vector (Known) and system matrix (SYSMAT) np.ndarray with imposed boundary conditions.
+        """
+        
+        known,sysmat = ndarrays
+        # Get boundary condition values.
+        if self.coolant.operations["INTIAL"] == 1:
+            # inlet pressure.
+            p_inl = self.coolant.operations["PREINL"]
+            # inlet temperature.
+            T_inl = self.coolant.operations["TEMINL"]
+            # outlet pressure.
+            p_out = self.coolant.operations["PREOUT"]
+            # outlet temperature: to be assigned if outlet velocity is negative.
+            T_out = self.coolant.operations["TEMOUT"]
+        else:
+            # get from file with interpolation in time.
+            [flow_par, flagSpecfield] = get_from_xlsx(
+                conductor,
+                path,
+                self,
+                "INTIAL",
+                self.coolant.operations["INTIAL"]
+            )
+            print(
+                f"""flagSpecfield == {flagSpecfield}: still to be decided if 
+            it is useful and if yes still to be defined\n"""
+            )
+            # inlet pressure.
+            p_inl = flow_par[2]
+            # inlet temperature.
+            T_inl = flow_par[0]
+            # outlet pressure.
+            p_out = flow_par[3]
+            # outlet temperature: to be assigned if outlet velocity is negative.
+            T_out = flow_par[1]
+        
+        # ALIAS
+        _, inl_p_idx, inl_t_idx = self.inl_idx # tuple unpack
+        _, out_p_idx, out_t_idx = self.out_idx # tuple unpack
+        main_d_idx = conductor.dict_band["Main_diag"]
+        flow_dir = self.channel.flow_dir[0]
+        velocity = self.coolant.dict_node_pt["velocity"]
+        
+        # Assign BC
+        if flow_dir == "forward":
+            # p_inl
+            sysmat[:,inl_p_idx.forward] = 0.0
+            # main diagonal.
+            sysmat[main_d_idx,inl_p_idx.forward] = 1.0
+            known[inl_p_idx.forward] = p_inl
+            # p_out
+            sysmat[:,out_p_idx.forward] = 0.0
+            # main diagonal.
+            sysmat[main_d_idx,out_p_idx.forward] = 1.0
+            known[out_p_idx.forward] = p_out
+            # T_inl
+            if velocity[0] > 0:
+                sysmat[:,inl_t_idx.forward] = 0.0
+                # main diagonal.
+                sysmat[main_d_idx,inl_t_idx.forward] = 1.0
+                known[inl_t_idx.forward] = T_inl
+            # T_out
+            if velocity[-1] < 0:
+                sysmat[:,out_t_idx.forward] = 0.0
+                # main diagonal.
+                sysmat[main_d_idx,out_t_idx.forward] = 1.0
+                known[out_t_idx.forward] = T_out
+        elif flow_dir == "backward":
+            # p_inl
+            sysmat[:,inl_p_idx.backward] = 0.0
+            # main diagonal.
+            sysmat[main_d_idx,inl_p_idx.backward] = 1.0
+            known[inl_p_idx.backward] = p_inl
+            # p_out
+            sysmat[:,out_p_idx.backward] = 0.0
+            # main diagonal.
+            sysmat[main_d_idx,out_p_idx.backward] = 1.0
+            known[out_p_idx.backward] = p_out
+            # T_inl
+            if velocity[-1] < 0:
+                sysmat[:,inl_t_idx.backward] = 0.0
+                # main diagonal.
+                sysmat[main_d_idx,inl_t_idx.backward] = 1.0
+                known[inl_t_idx.backward] = T_inl
+            # T_out
+            if velocity[0] > 0:
+                sysmat[:,out_t_idx.backward] = 0.0
+                # main diagonal.
+                sysmat[main_d_idx,out_t_idx.backward] = 1.0
+                known[out_t_idx.backward] = T_out
+
+        return known,sysmat
+
+    def impose_inl_p_out_v(
+        self,
+        ndarrays:tuple,
+        conductor:object,
+        path:str,
+        )->tuple:
+        
+        """Method that imposes a inlet pressure and outlet velocity as boundary conditions for the thermal hydraulic problem. Imposed quantities are:
+            * inlet pressure
+            * inlet temperature (or outlet temperature if backflow)
+            * outlet velocity
+        The method suitably accounts for forward (left to right)/backward (right to left) flow as well as for back flow (part of the fluid moves towards the inlet due to a pressure build up).
+
+        Args:
+            ndarrays (tuple): collection of known term vector (Known) and system matrix (SYSMAT) np.ndarray on wich impose the boundary conditions.
+            conductor (Conductor): object with all the information of the conductor.
+            path (str): path of the auxiliary input file with the values for the boundary conditions (if INTIAL = -2).
+
+        Returns:
+            tuple: collection of known term vector (Known) and system matrix (SYSMAT) np.ndarray with imposed boundary conditions.
+        """
+
+        # INLET AND OUTLET RESERVOIRS, INLET CONDITIONS AND FLOW SPECIFIED
+        known,sysmat = ndarrays
+        # Get boundary condition values.
+        if self.coolant.operations["INTIAL"] == 2:
+            # outlet mass flow rate.
+            mfr_out = self.coolant.operations["MDTOUT"]
+            # inlet pressure.
+            p_inl = self.coolant.operations["PREINL"]
+            # inlet temperature.
+            T_inl = self.coolant.operations["TEMINL"]
+            # outlet temperature: to be assigned if outlet velocity is negative.
+            T_out = self.coolant.operations["TEMOUT"]
+        else:  # N.B va aggiustato per renderlo conforme caso positivo!
+            # all values from flow_dummy.xlsx: call get_from_xlsx.
+            [flow_par, flagSpecfield] = get_from_xlsx(
+                conductor,
+                path,
+                self,
+                "INTIAL",
+                self.coolant.operations["INTIAL"],
+            )
+            print(
+                f"""flagSpecfield == {flagSpecfield}: still to be decided if it 
+            useful and if yes still to be defined\n"""
+            )
+            mfr_out = flow_par[3]  # inlet mass flow rate.
+            p_inl = flow_par[2]  # inlet pressure.
+            T_inl = flow_par[0]  # inlet temperature.
+            # outlet temperature: to be assigned if outlet velocity is negative.
+            T_out = flow_par[1]
+
+        # ALIAS
+        _, inl_p_idx, inl_t_idx = self.inl_idx # tuple unpack
+        out_v_idx, _, out_t_idx = self.out_idx # tuple unpack
+        main_d_idx = conductor.dict_band["Main_diag"]
+        flow_dir = self.channel.flow_dir[0]
+        velocity = self.coolant.dict_node_pt["velocity"]
+        density = self.coolant.dict_node_pt["total_density"]
+        cross_section = self.channel.inputs["CROSSECTION"]
+        
+        # Assign BC
+        if flow_dir == "forward":
+            # Flow direction from x = 0 to x = L.
+            # v_out
+            sysmat[:,out_v_idx.forward] = 0.0
+            # main diagonal.
+            sysmat[main_d_idx,out_v_idx.forward] = 1.0
+            known[out_v_idx.forward] = (mfr_out / density[-1] / cross_section)
+            # p_inl
+            sysmat[0:, inl_p_idx.forward] = 0.0
+            # main diagonal.
+            sysmat[main_d_idx, inl_p_idx.forward] = 1.0
+            known[inl_p_idx.forward] = p_inl
+            # T_inl
+            if velocity[0] > 0:
+                sysmat[:,inl_t_idx.forward] = 0.0
+                # main diagonal.
+                sysmat[main_d_idx,inl_t_idx.forward] = 1.0
+                known[inl_t_idx.forward] = T_inl
+            # T_out (T_inl if mfr_out < 0)
+            if velocity[-1] < 0:
+                sysmat[:,out_t_idx.forward] = 0.0
+                # main diagonal.
+                sysmat[main_d_idx,out_t_idx.forward] = 1.0
+                known[out_t_idx.forward] = T_out
+        elif flow_dir == "backward":
+            # Flow direction from x = L to x = 0.
+            # v_out
+            sysmat[:,out_v_idx.backward] = 0.0
+            # main diagonal.
+            sysmat[main_d_idx,out_v_idx.backward] = 1.0
+            known[out_v_idx.backward] = (mfr_out / density[0] / cross_section)
+            # p_inl
+            sysmat[0:, inl_p_idx.backward] = 0.0
+            # main diagonal.
+            sysmat[main_d_idx, inl_p_idx.backward] = 1.0
+            known[inl_p_idx.backward] = p_inl
+            # T_inl
+            if velocity[-1] < 0:
+                sysmat[:,inl_t_idx.backward] = 0.0
+                # main diagonal.
+                sysmat[main_d_idx,inl_t_idx.backward] = 1.0
+                known[inl_t_idx.backward] = T_inl
+            # T_out
+            if velocity[0] > 0:
+                sysmat[:,out_t_idx.backward] = 0.0
+                # main diagonal.
+                sysmat[main_d_idx,out_t_idx.backward] = 1.0
+                known[out_t_idx.backward] = T_out
+        
+        return known,sysmat
+
+    def impose_inl_v_out_p(
+        self,
+        ndarrays:tuple,
+        conductor:object,
+        path:str,
+        )->tuple:
+        
+        """Method that imposes a inlet velocity and outlet pressure as boundary conditions for the thermal hydraulic problem. Imposed quantities are:
+            * inlet velocity
+            * inlet temperature (or outlet temperature if backflow)
+            * outlet pressure
+        The method suitably accounts for forward (left to right)/backward (right to left) flow as well as for back flow (part of the fluid moves towards the inlet due to a pressure build up).
+
+        Args:
+            ndarrays (tuple): collection of known term vector (Known) and system matrix (SYSMAT) np.ndarray on wich impose the boundary conditions.
+            conductor (Conductor): object with all the information of the conductor.
+            path (str): path of the auxiliary input file with the values for the boundary conditions (if INTIAL = -3).
+
+        Returns:
+            tuple: collection of known term vector (Known) and system matrix (SYSMAT) np.ndarray with imposed boundary conditions.
+        """
+
+        known,sysmat = ndarrays
+        # Get boundary condition values.
+        if self.coolant.operations["INTIAL"] == 3:
+            # outlet mass flow rate.
+            mfr_inl = self.coolant.operations["MDTIN"]
+            # outlet pressure.
+            p_out = self.coolant.operations["PREOUT"]
+            # inlet temperature.
+            T_inl = self.coolant.operations["TEMINL"]
+            # outlet temperature: to be assigned if outlet velocity is negative.
+            T_out = self.coolant.operations["TEMOUT"]
+        else:  # N.B va aggiustato per renderlo conforme caso positivo!
+            # all values from flow_dummy.xlsx: call get_from_xlsx.
+            [flow_par, flagSpecfield] = get_from_xlsx(
+                conductor,
+                path,
+                self,
+                "INTIAL",
+                self.coolant.operations["INTIAL"],
+            )
+            print(
+                f"""flagSpecfield == {flagSpecfield}: still to be decided if it 
+            useful and if yes still to be defined\n"""
+            )
+            mfr_inl = flow_par[3]  # inlet mass flow rate.
+            p_out = flow_par[2]  # outlet pressure.
+            T_inl = flow_par[0]  # inlet temperature.
+            # outlet temperature: to be assigned if outlet velocity is negative.
+            T_out = flow_par[1]
+
+        # ALIAS
+        inl_v_idx, _, inl_t_idx = self.inl_idx # tuple unpack
+        _, out_p_idx, out_t_idx = self.out_idx # tuple unpack
+        main_d_idx = conductor.dict_band["Main_diag"]
+        flow_dir = self.channel.flow_dir[0]
+        velocity = self.coolant.dict_node_pt["velocity"]
+        density = self.coolant.dict_node_pt["total_density"]
+        cross_section = self.channel.inputs["CROSSECTION"]
+        
+        # Assign BC
+        if flow_dir == "forward":
+            # Flow direction from x = 0 to x = L.
+            # v_inl
+            sysmat[:,inl_v_idx.forward] = 0.0
+            # main diagonal.
+            sysmat[main_d_idx,inl_v_idx.forward] = 1.0
+            known[inl_v_idx.forward] = (mfr_inl / density[0] / cross_section)
+            # p_out
+            sysmat[0:, out_p_idx.forward] = 0.0
+            # main diagonal.
+            sysmat[main_d_idx, out_p_idx.forward] = 1.0
+            known[out_p_idx.forward] = p_out
+            # T_inl
+            if velocity[0] > 0:
+                sysmat[:,inl_t_idx.forward] = 0.0
+                # main diagonal.
+                sysmat[main_d_idx,inl_t_idx.forward] = 1.0
+                known[inl_t_idx.forward] = T_inl
+            # T_out (T_inl if mfr_inl < 0)
+            if velocity[-1] < 0:
+                sysmat[:,out_t_idx.forward] = 0.0
+                # main diagonal.
+                sysmat[main_d_idx,out_t_idx.forward] = 1.0
+                known[out_t_idx.forward] = T_out
+        elif flow_dir == "backward":
+            # Flow direction from x = L to x = 0.
+            # v_inl
+            sysmat[:,inl_v_idx.backward] = 0.0
+            # main diagonal.
+            sysmat[main_d_idx,inl_v_idx.backward] = 1.0
+            known[inl_v_idx.backward] = (mfr_inl / density[-1] / cross_section)
+            # p_out
+            sysmat[0:, out_p_idx.backward] = 0.0
+            # main diagonal.
+            sysmat[main_d_idx, out_p_idx.backward] = 1.0
+            known[out_p_idx.backward] = p_out
+            # T_inl
+            if velocity[-1] < 0:
+                sysmat[:,inl_t_idx.backward] = 0.0
+                # main diagonal.
+                sysmat[main_d_idx,inl_t_idx.backward] = 1.0
+                known[inl_t_idx.backward] = T_inl
+            # T_out
+            if velocity[0] > 0:
+                sysmat[:,out_t_idx.backward] = 0.0
+                # main diagonal.
+                sysmat[main_d_idx,out_t_idx.backward] = 1.0
+                known[out_t_idx.backward] = T_out
+        
+        return known,sysmat
 
 # End class FluidComponent.
