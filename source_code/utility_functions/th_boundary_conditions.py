42b575bf15accee060aca85b19db4c299ee46614
Daniele Placido <daniele.placido@polito.it>
2023-06-08 11:24:34 +0200

    feat: new pyhton module th_boundary_conditions.py
    New python module with functions used to apply boundary conditions to the thermal hydraulic problem. It is used to carry out refactoring of function step.
    
    new file:   utility_functions/th_boundary_conditions.py

---
+++ source_code/utility_functions/th_boundary_conditions.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+from collections import namedtuple
+from typing import Union, NamedTuple
+
+from utility_functions.auxiliary_functions import get_from_xlsx
+
+from fluid_component import FluidComponent
+from conductor import Conductor
