cd14bef7dfcae480d1be9c4b212669d60e22575f
DanielePlacido <daniele.placido@polito.it>
2024-05-02 12:28:40 +0200

    chore: call method update_contact_perimeters in adaptive_mesh
    Updated function adaptive_mesh calling method update_contact_perimeters of
    class Conductor, used to update the contact perimeter between FluidComponent
    objects, FluidComponent and SolidComponent objects and between SolidComponent
    objects on the new mesh.
    
    New input argumetn evironment in function adaptive mesh needed to call method
    update_contact_perimeters.
    
    Docstring updated accordingly.
    
    modified:   utility_functions/adaptive_mesh_functions.py

--- source_code/utility_functions/adaptive_mesh_functions.py
+++ source_code/utility_functions/adaptive_mesh_functions.py
@@ -23,11 +23,12 @@ HARD_NODE = True # an hard node is a node that belong to the initial mesh
 # False for soft nodes.
 SOFT_NODE = False
 
-def adaptive_mesh(conductor:Conductor)->Conductor:
+def adaptive_mesh(conductor:Conductor,environment:object)->Conductor:
     """Function that manages the mesh adaptivity according to an algorithm based on quench front detection. When the temperature of an instance of class StackComponent or StrandMixedComponent crosses the current sharing temperature, a quench front is found. The regions in the nearby a quench front are typically characterized by strong gradients and needs mesh refinement. Thus, in those regions the mesh is locally refined adding (soft) nodes. Since the quench front propagates, the added soft nodes may also be removed when they are no longer needed. This process is called mesh coarsening. For more info on the algorithm contact laura.savoldi@polito.it.
 
     Args:
         conductor (Conductor): object with all information to carry out mesh refinement.
+        environment (object): object with all the info that characterize the environment.
 
     Returns:
         Conductor: instance of class conductor with updated dictionaryes as below.
@@ -107,6 +108,11 @@ def adaptive_mesh(conductor:Conductor)->Conductor:
                 conductor.grid_features["zcoord"],
                 conductor.inventory["StrandComponent"].number,
             )
+        
+        # Update contact perimeters calling method update_contact_perimeters
+        conductor.dict_interf_peri = conductor.update_contact_perimeters(
+            environment
+        )
 
         return conductor
     else:
