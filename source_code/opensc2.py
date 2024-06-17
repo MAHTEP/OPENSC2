378c0926ca29369900b1966fe85ab5fd274c963c
DanielePlacido <daniele.placido@polito.it>
2024-06-17 17:33:17 +0200

    refactor: run simulation without GUI
    Removed the instance of class OPENSC2_GUI and replaced it with equivalent
    lines of code that allows to run simulation without the Graphical User
    Interface.
    
    modified:   opensc2.py

--- source_code/opensc2.py
+++ source_code/opensc2.py
@@ -1,16 +1,48 @@
+import yaml
 import logging
 import logging.config
 
-from opensc2_gui import OPENSC2_GUI
+from simulation import Simulation
 
-# from opensc2_gui_simpl import OPENSC2_GUI
+if __name__ == "__main__":
 
-logging.config.fileConfig(fname="logging_opensc2.conf", disable_existing_loggers=True)
+    logging.config.fileConfig(fname="logging_opensc2.conf", disable_existing_loggers=True)
 
-# Get the logger specified in the file, this will be the parent logger.
-logger = logging.getLogger("opensc2Logger")
+    # Get the logger specified in the file, this will be the parent logger.
+    logger = logging.getLogger("opensc2Logger")
 
-# make an instance of class OPENSC2_GUI (cdp, 12/2020)
-gui = OPENSC2_GUI()
-# Infinite loop of the main_window to start the program (cdp, 11/2020)
-gui.main_window.mainloop()
+    # Open .json file with paths to the input files of the simulation and the 
+    # path to the folder where to save the output of the simulation.
+    with open("io_path.yaml","r") as read_file:
+        io_path = yaml.safe_load(read_file)
+
+    # Create an instance of class Simulation
+    simulation = Simulation(io_path["input"])
+
+    # Save the path to the folder where to save the output of the simulation 
+    # in an attribute of class Simulation
+    simulation.dict_path["Main_dir"] = io_path["output"]
+
+    # Create and instance of class Conductor for each user defined conductor.
+    simulation.conductor_instance()
+    # Create the whole tree of folders to store the simulation data invoking 
+    # method Simulation_result_manager.
+    simulation.simulation_folders_manager()
+    # Save the input files in read only as metadata of the simulation outcome.
+    simulation.save_input_files()
+    # Initialize each user defined conductor.
+    simulation.conductor_initialization()
+    # Solve the linear system of equations at each time steps.
+    simulation.conductor_solution()
+    # Create plots of time evlustions and spatial distributions according to 
+    # user requirements.
+    simulation.conductor_post_processing()
+    # End simulation message.
+    messaggio = (
+        "Simulation called "
+        + simulation.transient_input["SIMULATION"]
+        + " ends.\n"
+        + "End of data processing and saving of figures.\n"
+    )
+    
+    print(messaggio)
