4651fc044c49c9228a98ca9868d205c36b5e27db
DanielePlacido <daniele.placido@polito.it>
2024-06-23 13:54:10 +0200

    refactor: function electric_transient_solution
    Replaced call to method conductor.electric_preprocessing with the more
    appropriate and coherent method conductor.electric_preprocessing_static_mesh
    to update at each electric time step only the strictly necessary ndarrays, i.e
    the electric resistance matrix and the electric stiffness matrix.
    
    modified:   utility_functions/electric_auxiliary_functions.py

--- source_code/utility_functions/electric_auxiliary_functions.py
+++ source_code/utility_functions/electric_auxiliary_functions.py
@@ -244,15 +244,13 @@ def electric_transient_solution(conductor: object):
         # operating_condition_em that evaluates the operating current at the 
         # current electric time step for each conductor component.
         conductor.eval_total_operating_current()
-        # Evaluate all matrices needed to solve the electromagnetic problem.
-        # N.B. it is not needed to evaluate at each time step the inductance 
-        # matrix, only the resistance matrix should be updated at each time 
-        # step: to be improved with refactoring. A good optimization would be 
+        # Update electric resistance matrix and electric stiffness matrix.
+        # N.B. to be improved with refactoring. A good optimization would be 
         # to actually update only the resistivity of the superconductor since 
         # it depends also from the current, and the electrical resistivity of 
         # the copper since it also depends on the magnetic fields. Other 
         # materials should be updated only in the thermal loop.
-        conductor.electric_preprocessing()
+        conductor.electric_preprocessing_static_mesh()
 
         electric_stiffness_matrix = conductor.electric_stiffness_matrix.copy()
         # Final form of the electric stiffness matrix
